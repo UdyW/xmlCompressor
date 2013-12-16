285271fd2815a69c659003b932bc0bcdc4ce5c3f
UdyW <udayanthaw@yahoo.com>
2013-12-16 16:36:02 +0000

    Merge branch 'master' of https://github.com/UdyW/xmlCompressor
    from the github

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
+/*define targets to the receiving requests on the server*/ 
 var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 
-server.start(router.route, handle);
+server.start(router.route, handle); //start listning to the port
